4d5623f3e890a54b609c8b193fb0834d4ae22cbd
Tourtelier Eliot <eliot.tourtelier@gmail.com>
2025-10-12 17:26:59 +0000

    reglage de bug

---
+++ Eliot-tourtelier/main.c
@@ -0,0 +1,43 @@
+// Eliot Tourtelier B1-b
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+	int main()
+{
+	srand(time(NULL));
+	int random_number = rand() % 100 + 1;
+	int guess;
+	while(guess != random_number)
+	{
+		printf("Choisissez un nombre entre 1 et 100 : \n");
+		scanf("%d", &guess);
+		if(guess <= 101 && guess >= 0 )
+		{
+			if(guess < random_number)
+			{
+				printf("C'est plus que %d !\n", guess);
+			}
+			else
+			{
+				printf("C'est moins que %d !\n", guess);
+			}
+		}
+		else 
+		{
+			printf("Saisi incorrect\n");
+		}
+	}
+	printf("Gagné !\nLe nombre était bien %d\n", guess);
+	char rejouer[10];
+	printf("Voulez-vous rejouer ?\n Y/N:\n");
+	getchar();
+	scanf("%9s", rejouer);
+	if(strcmp(rejouer, "y") == 0 || strcmp(rejouer, "Y") == 0 || strcmp(rejouer,"yes") == 0 || strcmp(rejouer, "oui") == 0)
+	{
+		return main();
+	}
+	exit(0);
+}
